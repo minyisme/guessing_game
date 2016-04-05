ecc9014193734f21c05ca2ff12a08b45d91830c9
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 15:51:08 -0700

    With comments and tracks number of guesses

--- game.py
+++ game.py
@@ -4,35 +4,33 @@ import random
 print "Hi there!"
 name = raw_input("What is your name?")
 
-print "Hi %s!" %(name)
+print "Hi %s!" % (name)
 
 raw_guess = raw_input("Choose a number between 1 and 100: ")
 
+# creates list of valid guesses
 valid_guesses = range(1,101)
-
 new_list = []
 
 for number in valid_guesses:
     new_list.append(str(number))
 
-#issues here
-# while raw_guess not in new_list:
-#     raw_guess = raw_input("That's invalid. Guess again.")
-
-# guess = raw_guess
-
 r_number = random.randint(1,100)
 
 print r_number
 
+# function validates if guess is a valid number between 1 and 100
 def validate_guess(raw_guess):
     while raw_guess not in new_list:
         raw_guess = raw_input("That's invalid. Guess again.")
     guess = int(raw_guess)
     return guess
 
+
+# checks if guess is the random number
 final_guess = validate_guess(raw_guess)
-while final_guess != r_number:
+total_guess = 1
+while final_guess != r_number:    
     if final_guess > r_number:
         print "Guess is too high."
     elif final_guess < r_number:
@@ -41,9 +39,10 @@ while final_guess != r_number:
     while raw_guess not in new_list:
         raw_guess = raw_input("That's invalid. Guess again.")
     final_guess = int(raw_guess)
+    total_guess += 1
 
 
 
-print "Congratulations! You have guessed the number."
+print "Congratulations! You have guessed the number. It took you %i guesses." % (total_guess)
 
 
